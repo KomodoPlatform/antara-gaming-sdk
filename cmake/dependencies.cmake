df2f692f7a424560ddf0a2c43a0c2092feb8572f
milerius <rmscastle@gmail.com>
2019-09-10 12:52:08 +0200

    feat(setup): avoid unwanted extra target from doctest

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,6 +1,7 @@
 ##! Dependancies
 include(FetchContent)
 
+set(DOCTEST_WITH_MAIN_IN_STATIC_LIB OFF CACHE BOOL "Override option" FORCE)
 FetchContent_Declare(
         doctest
         URL https://github.com/onqtam/doctest/archive/2.3.3.tar.gz
