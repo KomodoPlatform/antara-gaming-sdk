cae15853f65414d1bae039b8826bd2316c5a77e8
Roman Sztergbaum <rmscastle@gmail.com>
2019-09-10 08:43:11 +0200

    feat(setup): add check filesystem support in dummy example

--- examples/dummy_examples/main.cpp
+++ examples/dummy_examples/main.cpp
@@ -1,7 +1,11 @@
+#include <filesystem>
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello world\n";
+
+    //! Check filesystem working
+    std::cout << std::filesystem::current_path() << std::endl;
     return 0;
 }
