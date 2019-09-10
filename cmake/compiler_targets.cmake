52609823d5c4abb343435e67c8bd80f0e4b37acb
Roman Sztergbaum <rmscastle@gmail.com>
2019-09-10 08:35:15 +0200

    feat(setup): add basis example to verify CMake configuration

---
+++ cmake/compiler_targets.cmake
@@ -0,0 +1,43 @@
+add_library(antara_error_settings INTERFACE)
+
+
+# Using namespaces causes CMake to error our in case of typos on the
+# consuming side, very important.
+add_library(antara::error_settings ALIAS antara_error_settings)
+
+##! We use only clang for simplicity
+target_compile_options(
+        antara_error_settings
+        INTERFACE
+        $<$<AND:$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:Clang>>:-Wall -Wextra -Wfatal-errors>
+        $<$<AND:$<PLATFORM_ID:Darwin>,$<CXX_COMPILER_ID:Clang>>:-Wall -Wextra -Wfatal-errors>
+        $<$<AND:$<PLATFORM_ID:Windows>,$<CXX_COMPILER_ID:Clang>>:/W4 /permissive->)
+
+##! We are using C++ 17 for all of our targets
+add_library(antara_defaults_features INTERFACE)
+add_library(antara::defaults_features ALIAS antara_defaults_features)
+target_compile_features(antara_defaults_features INTERFACE cxx_std_17)
+
+add_library(antara_optimize_settings INTERFACE)
+add_library(antara::optimize_settings ALIAS antara_optimize_settings)
+
+##! Msvc flags info
+# /Zi - Produces a program database (PDB) that contains type information and symbolic debugging information for use with the debugger.
+# /FS - Allows multiple cl.exe processes to write to the same .pdb file
+# /DEBUG - Enable debug during linking
+# /Od - Disables optimization
+# /Ox - Full optimization
+# /Oy- do not suppress frame pointers (recommended for debugging)
+
+target_compile_options(antara_optimize_settings INTERFACE
+        $<$<AND:$<CONFIG:Debug>,$<CXX_COMPILER_ID:Clang>,$<PLATFORM_ID:Linux>>:-O0 -g>
+        $<$<AND:$<CONFIG:Release>,$<CXX_COMPILER_ID:Clang>,$<PLATFORM_ID:Linux>>:-O2 -march=native>
+        $<$<AND:$<CONFIG:Debug>,$<CXX_COMPILER_ID:Clang>,$<PLATFORM_ID:Darwin>>:-O0 -g>
+        $<$<AND:$<CONFIG:Release>,$<CXX_COMPILER_ID:Clang>,$<PLATFORM_ID:Darwin>>:-O2 -march=native>
+        $<$<AND:$<CONFIG:Debug>,$<CXX_COMPILER_ID:Clang>,$<PLATFORM_ID:Windows>>:/Zi /FS /DEBUG /Od /MP /MDd /Oy->
+        $<$<AND:$<CONFIG:Release>,$<CXX_COMPILER_ID:Clang>,$<PLATFORM_ID:Windows>>:/O2 -DNDEBUG /MP>
+        )
+
+add_library(antara_default_settings INTERFACE)
+add_library(antara::default_settings ALIAS antara_default_settings)
+target_link_libraries(antara_default_settings INTERFACE antara::error_settings antara::optimize_settings antara::defaults_features)
