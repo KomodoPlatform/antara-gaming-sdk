375697c933064fa3be717eb3331cf0377fc7256d
milerius <rmscastle@gmail.com>
2019-09-10 18:51:16 +0200

    feat(setup): coverage clion and tools and upgrade license

--- cmake/antara.cmake
+++ cmake/antara.cmake
@@ -7,12 +7,23 @@
 
  macro(target_enable_coverage target)
      if (ENABLE_COVERAGE)
-         message("coverage enaled, configuring...")
-         target_compile_options(${target} PUBLIC
-                 $<$<AND:$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:Clang>>:--coverage>
-                 $<$<AND:$<PLATFORM_ID:Darwin>,$<CXX_COMPILER_ID:Clang>>:--coverage>)
-         target_link_options(${target} PUBLIC
-                 $<$<AND:$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:Clang>>:--coverage>
-                 $<$<AND:$<PLATFORM_ID:Darwin>,$<CXX_COMPILER_ID:Clang>>:--coverage>)
+         message(STATUS "coverage enaled, configuring...")
+         if (COVERAGE_CLION_TOOLS)
+             message(STATUS "clion coverage tools enabled")
+             target_compile_options(${target} PUBLIC
+                     $<$<AND:$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:Clang>>:--coverage>
+                     $<$<AND:$<PLATFORM_ID:Darwin>,$<CXX_COMPILER_ID:Clang>>:-fprofile-instr-generate -fcoverage-mapping>)
+             target_link_options(${target} PUBLIC
+                     $<$<AND:$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:Clang>>:--coverage>
+                     $<$<AND:$<PLATFORM_ID:Darwin>,$<CXX_COMPILER_ID:Clang>>:-fprofile-instr-generate -fcoverage-mapping>)
+         else ()
+             message(STATUS "regular coverage tools enabled")
+             target_compile_options(${target} PUBLIC
+                     $<$<AND:$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:Clang>>:--coverage>
+                     $<$<AND:$<PLATFORM_ID:Darwin>,$<CXX_COMPILER_ID:Clang>>:--coverage>)
+             target_link_options(${target} PUBLIC
+                     $<$<AND:$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:Clang>>:--coverage>
+                     $<$<AND:$<PLATFORM_ID:Darwin>,$<CXX_COMPILER_ID:Clang>>:--coverage>)
+         endif ()
      endif ()
  endmacro()
