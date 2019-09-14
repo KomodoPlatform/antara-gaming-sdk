6e4e877a50c719c0bbcd5ebd63677b9f11404e74
milerius <rmscastle@gmail.com>
2019-09-14 10:43:10 +0200

    feat(input): add key pressed event

---
+++ modules/event/antara/gaming/event/antara.event.key.pressed.tests.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * Copyright © 2013-2019 The Komodo Platform Developers.                      *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Komodo Platform software, including this file may be copied, modified,     *
+ * propagated or distributed except according to the terms contained in the   *
+ * LICENSE file                                                               *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+#include <doctest/doctest.h>
+#include "antara/gaming/event/key.pressed.hpp"
+
+namespace antara::gaming::event::tests
+{
+    TEST_SUITE("key pressed test suite")
+    {
+        TEST_CASE("can construct from a key")
+        {
+            event::key_pressed key_pressed_event{input::key::a};
+            CHECK_EQ(key_pressed_event.key_, input::key::a);
+        }
+    }
+}
