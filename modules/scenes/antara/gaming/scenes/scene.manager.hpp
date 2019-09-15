55013a677b48e810bc295251b8d6ecfe25062685
milerius <rmscastle@gmail.com>
2019-09-16 00:15:00 +0200

    feat(scenes): add minimal scenes systems with unit tests

---
+++ modules/scenes/antara/gaming/scenes/scene.manager.hpp
@@ -0,0 +1,56 @@
+
+/******************************************************************************
+ * Copyright © 2013-2019 The Komodo Platform Developers.                      *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Komodo Platform software, including this file may be copied, modified,     *
+ * propagated or distributed except according to the terms contained in the   *
+ * LICENSE file                                                               *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+#pragma once
+
+#include <stack>
+#include "antara/gaming/ecs/system.hpp"
+#include "antara/gaming/scenes/base.scene.hpp"
+#include "antara/gaming/scenes/change.scene.event.hpp"
+
+namespace antara::gaming::scenes
+{
+    class manager final : public ecs::logic_update_system<manager>
+    {
+    public:
+        using scene_ptr = std::unique_ptr<base_scene>;
+
+        manager(entt::registry &entity_registry, entt::dispatcher &dispatcher) noexcept;
+
+        //! Destructor
+        ~manager() noexcept final = default;
+
+        //! Public callbacks
+        void receive_key_pressed(const event::key_pressed &evt) noexcept;
+        void receive_key_released(const event::key_released &evt) noexcept;
+        void receive_change_scene(const event::change_scene &evt) noexcept;
+        //!
+        void update() noexcept final;
+
+        //! Public member function
+        void change_scene(scene_ptr &&scene, bool just_push_scene = false) noexcept;
+
+        bool previous_scene();
+
+        base_scene &current_scene() noexcept;
+
+    private:
+        std::stack<scene_ptr> scenes_;
+    };
+}
+
+REFL_AUTO(type(antara::gaming::scenes::manager))
