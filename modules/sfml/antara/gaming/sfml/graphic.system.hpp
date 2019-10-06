b74c5e60d33803a72112bc33f764bd001000e764
milerius <rmscastle@gmail.com>
2019-10-06 11:00:28 +0200

    fix(windows): compilation due to narrowing conversion

--- modules/sfml/antara/gaming/sfml/graphic.system.hpp
+++ modules/sfml/antara/gaming/sfml/graphic.system.hpp
@@ -51,7 +51,9 @@ namespace antara::gaming::sfml
         config::game_cfg &game_cfg_{entity_registry_.ctx<config::game_cfg>()};
         config::window_cfg &window_cfg_{game_cfg_.win_cfg};
         sf::RenderWindow window_{sf::VideoMode(window_cfg_.width, window_cfg_.height),
-                                 window_cfg_.title, (game_cfg_.win_cfg.is_fullscreen == true) ? sf::Style::Fullscreen : sf::Style::Default};
+                                 window_cfg_.title, game_cfg_.win_cfg.is_fullscreen
+                                                    ? static_cast<sf::Uint32>(sf::Style::Fullscreen) :
+                                                    static_cast<sf::Uint32>(sf::Style::Default)};
         sf::RenderTexture render_texture_;
         sf::Sprite render_texture_sprite_;
     };
