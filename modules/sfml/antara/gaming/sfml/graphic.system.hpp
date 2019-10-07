204278695f5b27344621ae7fdc651ed67751d0c8
naezith <naezith@gmail.com>
2019-10-07 16:53:43 +0300

    fix(graphics): resize glitch

--- modules/sfml/antara/gaming/sfml/graphic.system.hpp
+++ modules/sfml/antara/gaming/sfml/graphic.system.hpp
@@ -58,7 +58,7 @@ namespace antara::gaming::sfml
                                                     ? static_cast<sf::Uint32>(sf::Style::Fullscreen) :
                                                     static_cast<sf::Uint32>(sf::Style::Default)};
         sf::RenderTexture& render_texture_{this->entity_registry_.set<sf::RenderTexture>()};
-        sf::Sprite render_texture_sprite_;
+        std::unique_ptr<sf::Sprite> render_texture_sprite_;
 
         void reset_render_texture() noexcept;
     };
