c99bd5593c128a8947bcfb145c846b8438d9658d
milerius <rmscastle@gmail.com>
2019-10-16 09:29:33 +0200

    docs(event): continue api event doc

--- docs/source/api/event.rst
+++ docs/source/api/event.rst
@@ -1,2 +1,11 @@
 antara::gaming::event
 =====================
+
+antara::gaming::event::key_pressed
+-----------------------------------
+
+.. doxygenclass:: antara::gaming::event::key_pressed
+   :project: antara-gaming-sdk
+   :members:
+   :protected-members:
+   :private-members:
--- modules/event/antara/gaming/event/key.pressed.hpp
+++ modules/event/antara/gaming/event/key.pressed.hpp
@@ -21,20 +21,60 @@
 
 namespace antara::gaming::event
 {
+    /**
+     * @class key_pressed
+     * @brief triggered when pressing a key on the keyboard.
+     *
+     * @verbatim embed:rst:leading-asterisk
+     *      .. note::
+     *         This class is automatically reflected for scripting systems such as lua, python.
+     * @endverbatim
+     */
     struct key_pressed
     {
+        /**
+         * constructor with args
+         * @param key_ represents the keyboard key currently pressed
+         * @param alt_ is true if the alt key on the keyboard is pressed
+         * @param control_ is true if the keyboard control key is pressed
+         * @param shift_ is true if the keyboard shift_ key is pressed
+         * @param system_ is true if the keyboard system key is pressed
+         *
+         * @verbatim embed:rst:leading-asterisk
+         *      .. note::
+         *         Principal Constructor.
+         * @endverbatim
+         *
+         * Example:
+         * @code{cpp}
+         *          #include <entt/entity/registry.hpp>
+         *          #include <entt/dispatcher/dispatcher.hpp>
+         *          #include <antara/gaming/ecs/system.manager.hpp>
+         *
+         *          int main()
+         *          {
+         *              entt::registry entity_registry;
+         *              entt::dispatcher& dispatcher{registry.set<entt::dispatcher>()};
+         *              dispatcher.trigger<key_pressed>(input::key::a, false, false, false, false);
+         *          }
+         * @endcode
+         */
         key_pressed(input::key key_, bool alt_,
                     bool control_,
                     bool shift_,
                     bool system_) noexcept;
 
+        /**
+         * default constructor (for scripting systems convenience)
+         */
         key_pressed() noexcept;
 
-        antara::gaming::input::key key;
-        bool alt{false};
-        bool control{false};
-        bool shift{false};
-        bool system{false};
+
+        antara::gaming::input::key key; ///< key pressed
+        bool alt{false}; ///< is alt pressed at the same time.
+        bool control{false}; ///< is ctrl pressed at the same time.
+        bool shift{false}; ///< is shift pressed at the same time.
+        bool system{false}; ///< is system pressed at the same time.
     };
 }
 
