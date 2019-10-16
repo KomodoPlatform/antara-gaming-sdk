e395fc7f2e92d64420ab0971dc91dead480b3408
milerius <rmscastle@gmail.com>
2019-10-16 22:37:59 +0200

    feat(tutorials): improvements for tic tac toe step_1

--- tutorials/tic-tac-toe/step_1/tic-tac-toe.cpp
+++ tutorials/tic-tac-toe/step_1/tic-tac-toe.cpp
@@ -24,65 +24,70 @@ struct tic_tac_toe_constants
     const float grid_thickness{20.0f};
 };
 
-static inline entt::entity create_grid(entt::registry &registry) noexcept
+//! Contains all the function that will be used for logic  and factory
+namespace
 {
-    //! retrieve canvas information
-    auto[canvas_width, canvas_height] = registry.ctx<graphics::canvas_2d>().canvas.size;
+    //! Factory for creating a tic-tac-toe grid
+    entt::entity create_grid(entt::registry &registry) noexcept
+    {
+        //! retrieve canvas information
+        auto[canvas_width, canvas_height] = registry.ctx<graphics::canvas_2d>().canvas.size;
 
-    //! entity creation
-    auto grid_entity = registry.create();
+        //! entity creation
+        auto grid_entity = registry.create();
 
-    //! our vertices
-    std::vector<geometry::vertex> lines{8 * 4};
+        //! our vertices
+        std::vector<geometry::vertex> lines{8 * 4};
 
-    //! retrieve constants information
-    auto[nb_cells, cell_width, cell_height, grid_thickness] = registry.ctx<tic_tac_toe_constants>();
-    const auto half_thickness = grid_thickness * 0.5f;
+        //! retrieve constants information
+        auto[nb_cells, cell_width, cell_height, grid_thickness] = registry.ctx<tic_tac_toe_constants>();
+        const auto half_thickness = grid_thickness * 0.5f;
 
-    //! our loop to create the grid
-    for (std::size_t counter = 0, i = 0; i <= nb_cells; ++i, counter += 4 * 2) {
+        //! our loop to create the grid
+        for (std::size_t counter = 0, i = 0; i <= nb_cells; ++i, counter += 4 * 2) {
 
-        //! to avoid narrowing conversion
-        auto idx = static_cast<float>(i);
+            //! to avoid narrowing conversion
+            auto idx = static_cast<float>(i);
 
-        // First and last ones should be a bit inside, otherwise half of it is out of the screen
-        auto offset_x = 0.0f;
-        auto offset_y = 0.0f;
+            // First and last ones should be a bit inside, otherwise half of it is out of the screen
+            auto offset_x = 0.0f;
+            auto offset_y = 0.0f;
 
-        if (i == 0) {
-            offset_x += half_thickness;
-            offset_y += half_thickness;
-        } else if (i == nb_cells) {
-            offset_x -= half_thickness;
-            offset_y -= half_thickness;
+            if (i == 0) {
+                offset_x += half_thickness;
+                offset_y += half_thickness;
+            } else if (i == nb_cells) {
+                offset_x -= half_thickness;
+                offset_y -= half_thickness;
+            }
+
+            // Prepare lines
+
+            // Vertical
+            lines[counter].pos = {offset_x + idx * cell_width - half_thickness, 0.f};
+            lines[counter + 1].pos = {offset_x + idx * cell_width + half_thickness, 0.f};
+            lines[counter + 2].pos = {offset_x + idx * cell_width + half_thickness, canvas_height};
+            lines[counter + 3].pos = {offset_x + idx * cell_width - half_thickness, canvas_height};
+
+            // Horizontal
+            lines[counter + 4].pos = {offset_x + 0, offset_y + idx * cell_height - half_thickness};
+            lines[counter + 5].pos = {offset_x + canvas_width, offset_y + idx * cell_height - half_thickness};
+            lines[counter + 6].pos = {offset_x + canvas_width, offset_y + idx * cell_height + half_thickness};
+            lines[counter + 7].pos = {offset_x + 0, offset_y + idx * cell_height + half_thickness};
         }
 
-        // Prepare lines
+        //! assign the vertex array to the grid entity
+        registry.assign<geometry::vertex_array>(grid_entity, lines, geometry::vertex_geometry_type::quads);
 
-        // Vertical
-        lines[counter].pos = {offset_x + idx * cell_width - half_thickness, 0.f};
-        lines[counter + 1].pos = {offset_x + idx * cell_width + half_thickness, 0.f};
-        lines[counter + 2].pos = {offset_x + idx * cell_width + half_thickness, canvas_height};
-        lines[counter + 3].pos = {offset_x + idx * cell_width - half_thickness, canvas_height};
+        //! assign the game_scene tag to the grid_entity (_hs means hashed_string)
+        registry.assign<entt::tag<"game_scene"_hs>>(grid_entity);
 
-        // Horizontal
-        lines[counter + 4].pos = {offset_x + 0, offset_y + idx * cell_height - half_thickness};
-        lines[counter + 5].pos = {offset_x + canvas_width, offset_y + idx * cell_height - half_thickness};
-        lines[counter + 6].pos = {offset_x + canvas_width, offset_y + idx * cell_height + half_thickness};
-        lines[counter + 7].pos = {offset_x + 0, offset_y + idx * cell_height + half_thickness};
+        //! We want to draw the grid on the most deep layer, here 0.
+        registry.assign<graphics::layer<0>>(grid_entity);
+
+        //! we give back our fresh entity
+        return grid_entity;
     }
-
-    //! assign the vertex array to the grid entity
-    registry.assign<geometry::vertex_array>(grid_entity, lines, geometry::vertex_geometry_type::quads);
-
-    //! assign the game_scene tag to the grid_entity (_hs means hashed_string)
-    registry.assign<entt::tag<"game_scene"_hs>>(grid_entity);
-
-    //! We want to draw the grid on the most deep layer, here 0.
-    registry.assign<graphics::layer<0>>(grid_entity);
-
-    //! we give back our fresh entity
-    return grid_entity;
 }
 
 class game_scene final : public scenes::base_scene
