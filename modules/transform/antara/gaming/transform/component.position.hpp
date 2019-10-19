36d925192806cb055e428ff1f435cd53cefc4766
milerius <rmscastle@gmail.com>
2019-10-19 21:25:33 +0200

    fix: lgtm error

--- modules/transform/antara/gaming/transform/component.position.hpp
+++ modules/transform/antara/gaming/transform/component.position.hpp
@@ -38,6 +38,7 @@ namespace antara::gaming::transform
         position_2d() noexcept = default;
 
         position_2d(const position_2d &other) noexcept = default;
+        position_2d& operator=(const position_2d &other) noexcept = default;
 
         position_2d(math::vec2f pos) noexcept : math::vec2f(std::move(pos))
         {
