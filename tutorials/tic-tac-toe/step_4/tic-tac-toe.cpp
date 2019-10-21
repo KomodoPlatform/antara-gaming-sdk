74d8a6f814d46fb54ab642d88931f5f76a7fbb71
naezith <naezith@gmail.com>
2019-10-21 17:06:27 +0300

    docs(tic-tac-toe): capitilize comment lines

--- tutorials/tic-tac-toe/step_4/tic-tac-toe.cpp
+++ tutorials/tic-tac-toe/step_4/tic-tac-toe.cpp
@@ -43,26 +43,26 @@ namespace
     //! Factory for creating a tic-tac-toe grid
     entt::entity create_grid(entt::registry &registry) noexcept
     {
-        //! retrieve canvas information
+        //! Retrieve canvas information
         auto[canvas_width, canvas_height] = registry.ctx<graphics::canvas_2d>().canvas.size;
 
-        //! entity creation
+        //! Entity creation
         auto grid_entity = registry.create();
 
-        //! our vertices
+        //! Our vertices
         std::vector<geometry::vertex> lines{8 * 4};
 
-        //! retrieve constants information
+        //! Retrieve constants information
         auto[nb_cells, cell_width, cell_height, grid_thickness] = registry.ctx<tic_tac_toe_constants>();
         const auto half_thickness = grid_thickness * 0.5f;
 
-        //! our loop to create the grid
+        //! Our loop to create the grid
         for (std::size_t counter = 0, i = 0; i <= nb_cells; ++i, counter += 4 * 2) {
 
-            //! to avoid narrowing conversion
+            //! To avoid narrowing conversion
             auto idx = static_cast<float>(i);
 
-            //! first and last ones should be a bit inside, otherwise half of it is out of the screen
+            //! First and last ones should be a bit inside, otherwise half of it is out of the screen
             auto offset_x = 0.0f;
             auto offset_y = 0.0f;
 
@@ -74,31 +74,31 @@ namespace
                 offset_y -= half_thickness;
             }
 
-            //! prepare lines
+            //! Prepare lines
 
-            //! vertical
+            //! Vertical
             lines[counter + 0].pos = {offset_x + idx * cell_width - half_thickness, 0.f};
             lines[counter + 1].pos = {offset_x + idx * cell_width + half_thickness, 0.f};
             lines[counter + 2].pos = {offset_x + idx * cell_width + half_thickness, canvas_height};
             lines[counter + 3].pos = {offset_x + idx * cell_width - half_thickness, canvas_height};
 
-            //! horizontal
+            //! Horizontal
             lines[counter + 4].pos = {offset_x + 0, offset_y + idx * cell_height - half_thickness};
             lines[counter + 5].pos = {offset_x + canvas_width, offset_y + idx * cell_height - half_thickness};
             lines[counter + 6].pos = {offset_x + canvas_width, offset_y + idx * cell_height + half_thickness};
             lines[counter + 7].pos = {offset_x + 0, offset_y + idx * cell_height + half_thickness};
         }
 
-        //! assign the vertex array to the grid entity
+        //! Assign the vertex array to the grid entity
         registry.assign<geometry::vertex_array>(grid_entity, lines, geometry::vertex_geometry_type::quads);
 
-        //! assign the game_scene tag to the grid_entity (_hs means hashed_string)
+        //! Assign the game_scene tag to the grid_entity (_hs means hashed_string)
         registry.assign<entt::tag<"game_scene"_hs>>(grid_entity);
 
         //! We want to draw the grid on the most deep layer, here 0.
         registry.assign<graphics::layer<0>>(grid_entity);
 
-        //! we give back our fresh entity
+        //! We give back our fresh entity
         return grid_entity;
     }
 
@@ -181,10 +181,10 @@ public:
     tic_tac_toe_logic(entt::registry &registry, entt::entity grid_entity, std::vector<cell_state> board) noexcept
             : system(registry), grid_entity_(grid_entity), board_(std::move(board))
     {
-        //! stateless system
+        //! Stateless system
         this->disable();
 
-        //! subscribe to mouse_button event
+        //! Subscribe to mouse_button event
         this->dispatcher_.sink<event::mouse_button_pressed>().connect<&tic_tac_toe_logic::on_mouse_button_pressed>(
                 *this);
     }
@@ -196,39 +196,39 @@ public:
 
 private:
     //! Game logic
-    [[nodiscard]] bool is_current_player_win_the_game() const noexcept
+    [[nodiscard]] bool did_current_player_win_the_game() const noexcept
     {
         std::size_t row_count{0u}, column_count{0u}, diag1_count{0u}, diag2_count{0u};
         auto[nb_cells, cell_width, cell_height, _] = entity_registry_.ctx<tic_tac_toe_constants>();
         for (std::size_t i = 0; i < nb_cells; ++i) {
             for (std::size_t j = 0; j < nb_cells; ++j) {
-                //! check rows
+                //! Check rows
                 if (board_[i * nb_cells + j] == static_cast<cell_state>(player_turn_))
                     row_count++;
 
-                //! check columns
+                //! Check columns
                 if (board_[j * nb_cells + i] == static_cast<cell_state>(player_turn_))
                     column_count++;
             }
 
-            //! check condition
+            //! Check condition
             if (row_count >= nb_cells || column_count >= nb_cells) {
                 return true;
             }
 
-            //! reset rows and columns
+            //! Reset rows and columns
             row_count = 0u, column_count = 0u;
 
-            //! diag1 count
+            //! Diag1 count
             if (board_[i * nb_cells + i] == static_cast<cell_state>(player_turn_))
                 diag1_count++;
 
-            //! second diag count
+            //! Second diag count
             if (board_[i * nb_cells + nb_cells - i - 1] == static_cast<cell_state>(player_turn_))
                 diag2_count++;
         }
 
-        //! condition
+        //! Condition
         return diag1_count >= nb_cells || diag2_count >= nb_cells;
     }
 
@@ -255,7 +255,7 @@ private:
             make_screen(graphics::yellow, entity);
         };
 
-        if (is_current_player_win_the_game()) {
+        if (did_current_player_win_the_game()) {
             current_game_state_ = static_cast<game_state>(player_turn_);
             make_player_win_screen(grid_entity_);
         } else if (is_tie()) {
@@ -275,16 +275,16 @@ private:
         //! Cell is available ?
         if (index < board_.size() && board_[index] == cell_state::empty) {
 
-            //! change state of the cell to the current player
+            //! Change state of the cell to the current player
             board_[index] = static_cast<cell_state>(player_turn_);
 
-            //! create x or o based on the current player
+            //! Create x or o based on the current player
             player_turn_ == x ? create_x(entity_registry_, row, column) : create_o(entity_registry_, row, column);
 
-            //! check winning condition
+            //! Check winning condition
             check_winning_condition();
 
-            //! switch player
+            //! Switch player
             player_turn_ = (player_turn_ == player::x) ? player::o : player::x;
         }
     }
@@ -345,7 +345,7 @@ public:
     {}
 
 
-    //! our scene name
+    //! Our scene name
     std::string scene_name() noexcept final
     {
         return "game_scene";
