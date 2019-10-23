822d557b8e4717157f4237613024a89119dea3f9
milerius <rmscastle@gmail.com>
2019-10-23 15:14:23 +0200

    feat(geometry): add rectangle generic component

--- modules/geometry/antara/gaming/geometry/component.rectangle.hpp
+++ modules/geometry/antara/gaming/geometry/component.rectangle.hpp
@@ -16,12 +16,35 @@
 
 #pragma once
 
-#include <meta/sequence/list.hpp>
-#include "antara/gaming/geometry/component.circle.hpp"
-#include "antara/gaming/geometry/component.rectangle.hpp"
-#include "antara/gaming/geometry/component.vertex.hpp"
+#include <entt/entity/entity.hpp>
+#include <entt/entity/registry.hpp>
+#include "antara/gaming/graphics/component.color.hpp"
+#include "antara/gaming/transform/component.position.hpp"
+#include "antara/gaming/math/vector.hpp"
 
 namespace antara::gaming::geometry
 {
-    using components_list = doom::meta::list<circle, vertex, vertex_array, rectangle>;
+    struct rectangle
+    {
+        rectangle() noexcept = default;
+        rectangle(const rectangle& other) noexcept = default;
+
+        rectangle(math::vec2f size_) noexcept;
+        rectangle &operator=(const rectangle &other) noexcept = default;
+
+
+#ifdef ANTARA_LUA_SCRIPTING_ENABLED
+        using constructors = sol::constructors<rectangle(), rectangle(const rectangle &other), rectangle(math::vec2f)>;
+#endif
+        math::vec2f size{math::vec2f::scalar(50.f)};
+    };
+
+    entt::entity blueprint_rectangle(
+            entt::registry &registry,
+            math::vec2f size,
+            graphics::fill_color fill_color = graphics::white,
+            transform::position_2d pos = math::vec2f::scalar(0.f),
+            graphics::outline_color out_color = graphics::transparent) noexcept;
 }
+
+REFL_AUTO(type(antara::gaming::geometry::rectangle), field(size));
