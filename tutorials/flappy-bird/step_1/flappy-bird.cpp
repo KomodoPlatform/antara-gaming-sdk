b06fbe9917f29e4d5d059e918d7ab8f8533f1897
naezith <naezith@gmail.com>
2019-10-23 13:49:43 +0300

    feat(tutorial): better comments

--- tutorials/flappy-bird/step_1/flappy-bird.cpp
+++ tutorials/flappy-bird/step_1/flappy-bird.cpp
@@ -5,25 +5,25 @@
 //! For convenience
 using namespace antara::gaming;
 
-//! Our game world
+//! Game world
 struct flappy_bird_world : world::app
 {
     //! Our game entry point
     flappy_bird_world() noexcept
     {
-        //! Here we load our graphical system
+        //! Load our graphical system
         auto &graphic_system = system_manager_.create_system<sfml::graphic_system>();
 
-        //! Here we load our input system with the window from the graphical system
+        //! Load our input system with the window from the graphical system
         system_manager_.create_system<sfml::input_system>(graphic_system.get_window());
     }
 };
 
 int main()
 {
-    //! Here we declare our world
+    //! Declare our world
     flappy_bird_world game;
 
-    //! Here we run the game
+    //! Run the game
     return game.run();
 }
