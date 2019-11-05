e217a270058a1c9fbef3819affc371fac6a5da5f
milerius <rmscastle@gmail.com>
2019-11-05 10:54:44 +0100

    feat(nspv): add reproc not working

--- modules/blockchain/antara/gaming/blockchain/nspv.system.cpp
+++ modules/blockchain/antara/gaming/blockchain/nspv.system.cpp
@@ -15,19 +15,83 @@
  ******************************************************************************/
 
 #include <loguru.hpp>
+#include <future>
+#include <mutex>
 #include <antara/gaming/blockchain/nspv.system.hpp>
+#include <antara/gaming/event/fatal.error.hpp>
+
+class thread_safe_string_sink {
+public:
+    thread_safe_string_sink(std::string &out, std::mutex &mutex)
+            : out_(out), mutex_(mutex) {}
+
+    bool operator()(const uint8_t *buffer, unsigned int size) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        out_.append(reinterpret_cast<const char *>(buffer), size);
+        return true;
+    }
+
+private:
+    std::string &out_;
+    std::mutex &mutex_;
+};
+
 
 namespace antara::gaming::blockchain {
     nspv::nspv(entt::registry &registry, fs::path tools_path) noexcept :
             system(registry), tools_path_(std::move(tools_path)) {
         LOG_SCOPE_FUNCTION(INFO);
         DVLOG_F(loguru::Verbosity_INFO, "assets tool path: {}", tools_path_.string());
-        this->disable();
     }
 
-    void nspv::update() noexcept {}
+    void nspv::update() noexcept {
+        for (auto &&[coin, _] : registry_) {
+            std::lock_guard<std::mutex> lock(output_registry_.at(coin).output_mutex);
+            DVLOG_F(loguru::Verbosity_INFO, "nspv process output: \n{}", output_registry_.at(coin).output);
+            output_registry_.at(coin).output.clear();
+        }
+    }
 
     nspv::~nspv() noexcept {
         LOG_SCOPE_FUNCTION(INFO);
+        for (auto &&[_, background] : registry_) {
+            auto ec = background.stop(reproc::cleanup::terminate, reproc::milliseconds(2000), reproc::cleanup::kill,
+                                      reproc::infinite);
+            if (ec) {
+                VLOG_SCOPE_F(loguru::Verbosity_ERROR, "error: %s", ec.message().c_str());
+            }
+        }
     }
+
+    bool nspv::spawn_nspv_instance(const std::string &coin) noexcept {
+        LOG_SCOPE_FUNCTION(INFO);
+        auto it = registry_.emplace(coin, reproc::process{reproc::cleanup::terminate,
+                                                          reproc::milliseconds(2000),
+                                                          reproc::cleanup::kill,
+                                                          reproc::infinite}).first;
+        reproc::process &background = it->second;
+        std::array<std::string, 1> args = {"nspvs"};
+        auto ec = background.start(args, tools_path_.string().c_str());
+        if (ec) {
+            DVLOG_F(loguru::Verbosity_FATAL, "couldn't start nspv process with coin: {}, error: {}", coin,
+                    ec.message());
+            this->dispatcher_.trigger<event::fatal_error>(ec);
+            return false;
+        }
+        output_registry_.try_emplace(coin, background);
+        return true;
+    }
+
+    nspv_output::nspv_output(reproc::process &background) noexcept :
+            output(""),
+            output_mutex(),
+            async_drain(
+                    std::async(std::launch::async, [this, &background]() {
+                        thread_safe_string_sink sink(
+                                output,
+                                output_mutex);
+                        return background.drain(
+                                reproc::stream::out,
+                                sink);
+                    })) {}
 }
--- modules/blockchain/antara/gaming/blockchain/nspv.system.hpp
+++ modules/blockchain/antara/gaming/blockchain/nspv.system.hpp
@@ -17,20 +17,37 @@
 #pragma once
 
 #include <filesystem>
+#include <string>
+#include <future>
 #include <entt/entity/registry.hpp>
+#include <reproc++/reproc.hpp>
 #include <antara/gaming/core/real.path.hpp>
 #include <antara/gaming/ecs/system.hpp>
 
 namespace fs = std::filesystem;
 
 namespace antara::gaming::blockchain {
+
+    struct nspv_output
+    {
+        nspv_output(reproc::process& background) noexcept;
+        std::string output;
+        std::mutex output_mutex;
+        std::future<std::error_code> async_drain;
+    };
+
     class nspv final : public ecs::logic_update_system<nspv> {
     public:
         nspv(entt::registry &registry, fs::path tools_path = core::assets_real_path() / "tools") noexcept;
         void update() noexcept final;
+        bool spawn_nspv_instance(const std::string& coin) noexcept;
         ~nspv() noexcept final;
     private:
         std::filesystem::path tools_path_;
+        using nspv_registry = std::unordered_map<std::string, reproc::process>;
+        using async_drain_registry = std::unordered_map<std::string, nspv_output>;
+        nspv_registry registry_;
+        async_drain_registry output_registry_;
     };
 }
 
