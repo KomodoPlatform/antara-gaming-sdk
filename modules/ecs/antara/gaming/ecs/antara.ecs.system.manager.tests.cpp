55c19b84bccc1dd252fec1c22b51e329027ced1e
milerius <rmscastle@gmail.com>
2019-11-06 10:33:39 +0100

    feat(ecs): add create_system_rt

--- modules/ecs/antara/gaming/ecs/antara.ecs.system.manager.tests.cpp
+++ modules/ecs/antara/gaming/ecs/antara.ecs.system.manager.tests.cpp
@@ -18,35 +18,29 @@
 #include "antara/gaming/ecs/system.hpp"
 #include "antara/gaming/ecs/system.manager.hpp"
 
-class logic_concrete_system final : public antara::gaming::ecs::logic_update_system<logic_concrete_system>
-{
+class logic_concrete_system final : public antara::gaming::ecs::logic_update_system<logic_concrete_system> {
 public:
-    logic_concrete_system(entt::registry &registry) : system(registry)
-    {
+    logic_concrete_system(entt::registry &registry) : system(registry) {
 
     }
 
     logic_concrete_system() = default;
 
-    void update() noexcept final
-    {
+    void update() noexcept final {
 
     }
 
     ~logic_concrete_system() noexcept final = default;
 };
 
-class pre_concrete_system final : public antara::gaming::ecs::pre_update_system<pre_concrete_system>
-{
+class pre_concrete_system final : public antara::gaming::ecs::pre_update_system<pre_concrete_system> {
 public:
-    pre_concrete_system(entt::registry &registry) noexcept : system(registry)
-    {
+    pre_concrete_system(entt::registry &registry) noexcept : system(registry) {
 
     }
 
 
-    void update() noexcept final
-    {
+    void update() noexcept final {
 
     }
 
@@ -56,99 +50,91 @@ public:
 REFL_AUTO(type(logic_concrete_system))
 REFL_AUTO(type(pre_concrete_system))
 
-namespace antara::gaming::ecs::tests
-{
-    TEST_SUITE ("system manager test suite")
-    {
+namespace antara::gaming::ecs::tests {
+    TEST_CASE ("add system") {
         entt::registry registry;
-        entt::dispatcher& dispatcher{registry.set<entt::dispatcher>()};
+        entt::dispatcher &dispatcher{registry.set<entt::dispatcher>()};
         system_manager manager{registry};
         const system_manager &c_mgr = manager;
+        manager.start();
 
-        TEST_CASE ("add system")
-        {
-            manager.start();
-            CHECK_EQ(manager.nb_systems(), 0u);
-            manager.create_system<logic_concrete_system>();
-            CHECK(manager.has_system<logic_concrete_system>());
-            CHECK_EQ(manager.nb_systems(), 1u);
-            CHECK_EQ(manager.nb_systems(logic_concrete_system::get_system_type()), 1u);
-            manager.create_system<logic_concrete_system>();
-            CHECK_EQ(manager.nb_systems(), 1u);
-            CHECK_EQ(manager.nb_systems(logic_concrete_system::get_system_type()), 1u);
+        //! add
+                CHECK_EQ(manager.nb_systems(), 0u);
+        manager.create_system<logic_concrete_system>();
+                CHECK(manager.has_system<logic_concrete_system>());
+                CHECK_EQ(manager.nb_systems(), 1u);
+                CHECK_EQ(manager.nb_systems(logic_concrete_system::get_system_type()), 1u);
+        manager.create_system<logic_concrete_system>();
+                CHECK_EQ(manager.nb_systems(), 1u);
+                CHECK_EQ(manager.nb_systems(logic_concrete_system::get_system_type()), 1u);
 
-            //! evt
-            ecs::event::add_base_system evt(std::make_unique<pre_concrete_system>(registry));
-            manager.receive_add_base_system(evt);
-            manager.update();
-            CHECK_EQ(manager.nb_systems(), 2u);
-            CHECK(manager.mark_system<pre_concrete_system>());
-            manager.update();
-            CHECK_EQ(manager.nb_systems(), 1u);
-        }
+        //! evt
+        ecs::event::add_base_system evt(std::make_unique<pre_concrete_system>(registry));
+        manager.receive_add_base_system(evt);
+        manager.update();
+                CHECK_EQ(manager.nb_systems(), 2u);
+                CHECK(manager.mark_system<pre_concrete_system>());
+        manager.update();
+                CHECK_EQ(manager.nb_systems(), 1u);
 
-        TEST_CASE("remove system")
-        {
-            CHECK_EQ(manager.nb_systems(), 1u);
-            CHECK(manager.mark_system<logic_concrete_system>());
-            manager.update();
-            CHECK_FALSE(manager.has_system<logic_concrete_system>());
-            CHECK_EQ(manager.nb_systems(), 0u);
-            CHECK_FALSE(manager.mark_system<logic_concrete_system>());
-        }
+        manager.create_system_rt<pre_concrete_system>();
+                CHECK_EQ(manager.nb_systems(), 1u);
+        manager.update();
+                CHECK_EQ(manager.nb_systems(), 2u);
+                CHECK(manager.mark_system<pre_concrete_system>());
+        manager.update();
+                CHECK_EQ(manager.nb_systems(), 1u);
 
-        TEST_CASE("add multiple systems")
-        {
-            manager.load_systems<logic_concrete_system, pre_concrete_system>();
-            CHECK_EQ(manager.nb_systems(), 2u);
-            CHECK(manager.has_systems<logic_concrete_system, pre_concrete_system>());
-        }
 
-        TEST_CASE("update/enable/disable systems from specific type")
-        {
-            CHECK_EQ(manager.update_systems(pre_update), 1ull);
-            CHECK(manager.disable_system<pre_concrete_system>());
-            CHECK_EQ(manager.update_systems(pre_update), 0ull);
-            CHECK(manager.enable_system<pre_concrete_system>());
+        //! remove
+                CHECK_EQ(manager.nb_systems(), 1u);
+                CHECK(manager.mark_system<logic_concrete_system>());
+        manager.update();
+                CHECK_FALSE(manager.has_system<logic_concrete_system>());
+                CHECK_EQ(manager.nb_systems(), 0u);
+                CHECK_FALSE(manager.mark_system<logic_concrete_system>());
 
-            CHECK(manager.disable_systems<logic_concrete_system, pre_concrete_system>());
-            CHECK_EQ(manager.update(), 0ull);
-            CHECK(manager.enable_systems<logic_concrete_system, pre_concrete_system>());
-            CHECK_GE(manager.update(), 1ull);
-        }
 
-        TEST_CASE("get single system")
-        {
-            auto &logic_system = manager.get_system<logic_concrete_system>();
+        //! add multiple
+        manager.load_systems<logic_concrete_system, pre_concrete_system>();
+                CHECK_EQ(manager.nb_systems(), 2u);
+                CHECK(manager.has_systems<logic_concrete_system, pre_concrete_system>());
 
-            const auto& c_logic_system = c_mgr.get_system<logic_concrete_system>();
-            CHECK_EQ(logic_system.get_name(), "logic_concrete_system");
-            CHECK_EQ(c_logic_system.get_name(), "logic_concrete_system");
-        }
+        //! update/enable/disable systems from specific type
+                CHECK_EQ(manager.update_systems(pre_update), 1ull);
+                CHECK(manager.disable_system<pre_concrete_system>());
+                CHECK_EQ(manager.update_systems(pre_update), 0ull);
+                CHECK(manager.enable_system<pre_concrete_system>());
 
-        TEST_CASE("get multiple systems")
-        {
-            auto&& [lgc_sys, pre_sys] = manager.get_systems<logic_concrete_system, pre_concrete_system>();
-            CHECK_EQ(lgc_sys.get_name(), "logic_concrete_system");
-            CHECK_EQ(pre_sys.get_name(), "pre_concrete_system");
+                CHECK(manager.disable_systems<logic_concrete_system, pre_concrete_system>());
+                CHECK_EQ(manager.update(), 0ull);
+                CHECK(manager.enable_systems<logic_concrete_system, pre_concrete_system>());
+                CHECK_GE(manager.update(), 1ull);
 
-            auto&& [c_lgc_sys, c_pre_sys] = c_mgr.get_systems<logic_concrete_system, pre_concrete_system>();
-            CHECK_EQ(c_lgc_sys.get_name(), "logic_concrete_system");
-            CHECK_EQ(c_pre_sys.get_name(), "pre_concrete_system");
-        }
+        //! get single
+        auto &logic_system = manager.get_system<logic_concrete_system>();
 
-        TEST_CASE("marked multiple systems")
-        {
-            CHECK(manager.has_systems<logic_concrete_system, pre_concrete_system>());
-            CHECK(manager.mark_systems<logic_concrete_system, pre_concrete_system>());
-            manager.update();
-            CHECK_FALSE(manager.has_systems<logic_concrete_system, pre_concrete_system>());
-            CHECK_FALSE(manager.enable_systems<logic_concrete_system, pre_concrete_system>());
-            CHECK_FALSE(manager.disable_systems<logic_concrete_system, pre_concrete_system>());
-            CHECK_EQ(0ull, manager.update());
-            CHECK_EQ(0ull, manager.nb_systems());
-            //manager.get_systems<logic_concrete_system, pre_concrete_system>(); //UBSAN
-            //c_mgr.get_systems<logic_concrete_system, pre_concrete_system>(); // UBSAN
-        }
+        const auto &c_logic_system = c_mgr.get_system<logic_concrete_system>();
+                CHECK_EQ(logic_system.get_name(), "logic_concrete_system");
+                CHECK_EQ(c_logic_system.get_name(), "logic_concrete_system");
+
+        //! get multiple
+        auto&&[lgc_sys, pre_sys] = manager.get_systems<logic_concrete_system, pre_concrete_system>();
+                CHECK_EQ(lgc_sys.get_name(), "logic_concrete_system");
+                CHECK_EQ(pre_sys.get_name(), "pre_concrete_system");
+
+        auto&&[c_lgc_sys, c_pre_sys] = c_mgr.get_systems<logic_concrete_system, pre_concrete_system>();
+                CHECK_EQ(c_lgc_sys.get_name(), "logic_concrete_system");
+                CHECK_EQ(c_pre_sys.get_name(), "pre_concrete_system");
+
+        //! marked multiple
+                CHECK(manager.has_systems<logic_concrete_system, pre_concrete_system>());
+                CHECK(manager.mark_systems<logic_concrete_system, pre_concrete_system>());
+        manager.update();
+                CHECK_FALSE(manager.has_systems<logic_concrete_system, pre_concrete_system>());
+                CHECK_FALSE(manager.enable_systems<logic_concrete_system, pre_concrete_system>());
+                CHECK_FALSE(manager.disable_systems<logic_concrete_system, pre_concrete_system>());
+                CHECK_EQ(0ull, manager.update());
+                CHECK_EQ(0ull, manager.nb_systems());
     }
 }
