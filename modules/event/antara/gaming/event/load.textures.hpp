dc174f093f099e47f749479320359a05974a0af6
Roman Sztergbaum <rmscastle@gmail.com>
2019-11-22 09:58:10 +0100

    feat(resources): add load.textures.hpp

---
+++ modules/event/antara/gaming/event/load.textures.hpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+ * Copyright © 2013-2019 The Komodo Platform Developers.                      *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Komodo Platform software, including this file may be copied, modified,     *
+ * propagated or distributed except according to the terms contained in the   *
+ * LICENSE file                                                               *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace antara::gaming::event
+{
+    struct loading_settings
+    {
+        std::string texture_id;
+        bool smooth{true};
+    };
+    
+    struct load_textures
+    {
+        std::vector<loading_settings> textures_settings;
+    };
+}
