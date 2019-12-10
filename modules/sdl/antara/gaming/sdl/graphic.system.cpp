f91c784745f8c0d873bd894c4a02bdadad695afb
naezith <naezith@gmail.com>
2019-12-10 14:53:35 +0300

    feat(fps): fps cap

--- modules/sdl/antara/gaming/sdl/graphic.system.cpp
+++ modules/sdl/antara/gaming/sdl/graphic.system.cpp
@@ -41,7 +41,144 @@ namespace {
 }
 
 namespace antara::gaming::sdl {
+
+//The application time based timer
+    class LTimer
+    {
+    public:
+        //Initializes variables
+        LTimer()
+        {
+            //Initialize the variables
+            mStartTicks = 0;
+            mPausedTicks = 0;
+
+            mPaused = false;
+            mStarted = false;
+        }
+
+        //The various clock actions
+        void start()
+        {
+            //Start the timer
+            mStarted = true;
+
+            //Unpause the timer
+            mPaused = false;
+
+            //Get the current clock time
+            mStartTicks = SDL_GetTicks();
+            mPausedTicks = 0;
+        }
+        void stop()
+        {
+            //Stop the timer
+            mStarted = false;
+
+            //Unpause the timer
+            mPaused = false;
+
+            //Clear tick variables
+            mStartTicks = 0;
+            mPausedTicks = 0;
+        }
+        void pause()
+        {
+            //If the timer is running and isn't already paused
+            if( mStarted && !mPaused )
+            {
+                //Pause the timer
+                mPaused = true;
+
+                //Calculate the paused ticks
+                mPausedTicks = SDL_GetTicks() - mStartTicks;
+                mStartTicks = 0;
+            }
+        }
+        void unpause()
+        {
+            //If the timer is running and paused
+            if( mStarted && mPaused )
+            {
+                //Unpause the timer
+                mPaused = false;
+
+                //Reset the starting ticks
+                mStartTicks = SDL_GetTicks() - mPausedTicks;
+
+                //Reset the paused ticks
+                mPausedTicks = 0;
+            }
+        }
+
+        //Gets the timer's time
+        Uint32 getTicks()
+        {
+            //The actual timer time
+            Uint32 time = 0;
+
+            //If the timer is running
+            if( mStarted )
+            {
+                //If the timer is paused
+                if( mPaused )
+                {
+                    //Return the number of ticks when the timer was paused
+                    time = mPausedTicks;
+                }
+                else
+                {
+                    //Return the current time minus the start time
+                    time = SDL_GetTicks() - mStartTicks;
+                }
+            }
+
+            return time;
+        }
+
+        //Checks the status of the timer
+        bool isStarted()
+        {
+            //Timer is running and paused or unpaused
+            return mStarted;
+        }
+        bool isPaused()
+        {
+            //Timer is running and paused
+            return mPaused && mStarted;
+        }
+
+    private:
+        //The clock time when the timer started
+        Uint32 mStartTicks;
+
+        //The ticks stored when the timer was paused
+        Uint32 mPausedTicks;
+
+        //The timer status
+        bool mPaused;
+        bool mStarted;
+    };
+
     void graphic_system::update() noexcept {
+        //The frames per second timer
+        static LTimer capTimer;
+        static const int fps_cap = 30;
+
+        render();
+        //If frame finished early
+        int frameTicks = capTimer.getTicks();
+        if( frameTicks < fps_cap )
+        {
+            //Wait remaining time
+            SDL_Delay((1000 / fps_cap) - frameTicks);
+        }
+
+        //Start cap timer
+        capTimer.start();
+    }
+
+    void graphic_system::render() noexcept {
 #if defined(IMGUI_AND_SDL_ENABLED)
         ImGui::Render();
         ImGuiIO &io = ImGui::GetIO();
